fe2cb990909d91611f34ff558e855eeb88299d54
Justin de Vesine <justin@crowdfavorite.com>
2014-01-20 12:30:11 -0700

    bump version to 1.0.3-dev

--- cf-revision-manager.php
+++ cf-revision-manager.php
@@ -3,7 +3,7 @@
 Plugin Name: CF Revision Manager
 Plugin URI: http://crowdfavorite.com
 Description: Revision management functionality so that plugins can add metadata to revisions as well as restore that metadata from revisions
-Version: 1.0.2
+Version: 1.0.3-dev
 Author: Crowd Favorite
 Author URI: http://crowdfavorite.com 
 */
